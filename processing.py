09024233a22689af879efd3a1ed4f553ac31c6da
bhuchit <poochit1996@hotmail.com>
2022-06-07 01:00:14 +0700

    update: feature color filter & refactor code

--- processing.py
+++ processing.py
@@ -2,24 +2,40 @@
 import cv2
 import numpy as np
 
-def detect_yellow(img: bytes) -> bytes:
-    
-    cv2_img = cv2.imdecode(np.frombuffer(img, np.uint8), cv2.IMREAD_COLOR)
+def detect_color(img: bytes, h_min: int, h_max: int, s_min: int, v_min: int, h_red_min: int, h_red_max: int, check_red: bool) -> bytes:
     # Reading the image
-    # img = cv2.imread('image.png')
-
-    # convert to hsv colorspace
+    cv2_img = cv2.imdecode(np.frombuffer(img, np.uint8), cv2.IMREAD_COLOR)
+    
+    # convert to hsv color space
     hsv = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2HSV)
 
     # lower bound and upper bound for Green color
-    lower_bound = np.array([22, 100, 100])   
-    upper_bound = np.array([38, 255, 255])
+    # lower boundary RED color range values; Hue (0 - 10)
+    lower_bound = np.array([h_min, s_min, v_min])   
+    upper_bound = np.array([h_max, 255, 255])
 
-    # find the colors within the boundaries
-    mask = cv2.inRange(hsv, lower_bound, upper_bound)
-    cv2.imwrite('yellow.jpg', mask)
+    if check_red:
+        # upper boundary RED color range values; Hue (160 - 180)
+        lower_bound_red = np.array([h_red_min, s_min, v_min])   
+        upper_bound_red = np.array([h_red_max, 255, 255])
+
+        lower_mask = cv2.inRange(hsv, lower_bound, upper_bound)
+        upper_mask = cv2.inRange(hsv, lower_bound_red, upper_bound_red)
+
+        mask = lower_mask + upper_mask
+    else:
+        # find the colors within the boundaries
+        mask = cv2.inRange(hsv, lower_bound, upper_bound)
+
+    cv2.imwrite('mask.jpg', mask)
     
-    image_bytes = cv2.imencode('.jpg', mask)
+    img_bytes_mask = cv2.imencode('.jpg', mask)
+
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(cv2_img, cv2_img, mask= mask)
+    cv2.imwrite('res.jpg', res)
+
+    img_bytes_res = cv2.imencode('.jpg', res)
     
-    return image_bytes
+    return img_bytes_mask, img_bytes_res
 
